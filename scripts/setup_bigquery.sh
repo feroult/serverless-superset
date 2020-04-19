f2b8728b5c9f39711dfb7f3f665bb5ed061cbdfa
Andreas Wöhrl <info@volderette.de>
2020-04-19 10:24:27 +0200

    Removed duplicate brackets around PROJECT_ID

--- scripts/setup_bigquery.sh
+++ scripts/setup_bigquery.sh
@@ -4,6 +4,6 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
   --member "serviceAccount:${PROJECT_ID}@appspot.gserviceaccount.com" \
   --role "roles/bigquery.dataViewer"
 
-gcloud projects add-iam-policy-binding ${${PROJECT_ID}} \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
   --member "serviceAccount:${PROJECT_ID}@appspot.gserviceaccount.com" \
   --role "roles/bigquery.jobUser"
